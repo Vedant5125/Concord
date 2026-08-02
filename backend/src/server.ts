6894f7cd5e86ada03bc012231cfe80aaaeb73c49
Vedant5125 <vedantapraj5125@gmail.com>
2026-08-03 01:02:30 +0530

    added new routes

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,13 +1,19 @@
 import express from "express";
 import { prisma } from "./db.js";
 import publishContract from "./controller/publish.js"; 
+import listContracts from "./controller/listContracts.js";
+import interactionList from "./controller/listInteractions.js";
+import mockServer  from "./controller/mockServer.js"
 import "dotenv/config";
 
 const app = express();
 
 app.use(express.json({limit:"1mb"}));
 
-app.post("/api/publish",publishContract);
+app.post("/api/publish", publishContract);
+app.get("/api/listContracts", listContracts);
+app.get("/api/listInteractions", interactionList);
+app.post("/api/mockServer",mockServer);
 
 app.listen(3000, () => console.log('Concord backend running on port 3000'));
 
