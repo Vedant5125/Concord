13c1cdfa1cd5dff007a8cc058c57c86c3095eb92
Vedant5125 <vedantapraj5125@gmail.com>
2026-08-29 01:16:30 +0530

    added api-key header to apiClient

--- concord-cli/src/api.ts
+++ concord-cli/src/api.ts
@@ -3,7 +3,10 @@ import "dotenv/config"
 
 const apiClient = axios.create({
     baseURL: process.env.BASE_URL || "http://localhost:3000",
-    timeout: 10000
+    timeout: 10000,
+    headers:{
+        'user-key': process.env.CONCORD_API_KEY
+    }
 })
 
 export default apiClient;
